6a3f5442960703191ff28d5e10e323b0e2984ab5
Fade <fadepublicdvd@gmail.com>
2023-03-13 20:42:59 +0200

    prod

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero({ pageInfo }: Props) {
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
 
-      <Image
+      <img
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src={urlFor(pageInfo?.heroImage).url()}
         alt=""
