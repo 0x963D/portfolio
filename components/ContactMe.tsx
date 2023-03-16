0ca8fbdc757490785f0aac0edd325e4dd7ee3d25
Fade <fadepublicdvd@gmail.com>
2023-03-16 16:51:47 +0200

    updates contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -33,7 +33,7 @@ export default function ContactMe({}: Props) {
         <div className="space-y-1 md:space-y-3 lg:space-y-3 xl:space-y-3 2xl:space-y-5">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-darkGold h-7 w-7 animate-pulse" />
-            <p className="text-lg md:text-2xl lg:text-2xl">+4075 171 030</p>
+            <p className="text-lg md:text-2xl lg:text-2xl">+40757 171 030</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-darkGold h-7 w-7 animate-pulse" />
